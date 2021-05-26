b2ba22bb91a023e317902c17136117919f32b448
AbidHanif <qadriturabi12@gmail.com>
2021-05-26 14:56:25 +0500

    css designed

--- app.js
+++ app.js
@@ -24,17 +24,17 @@
 
 /************* Question no 4 */
 
-// var player1Name = prompt ("enter player 1 name")
-// var player2Name = prompt ("enter player 2 name")
-// var tossResult = Math.round(Math.random())
-// if(tossResult === 1){
-//     alert (player1Name.toUpperCase() + (" ") +"Selected HEAD and" + (" ") + "won the toss")
-// }
-// else{
-//     alert (player2Name.toUpperCase() + (" ") + "Selected Tail and" + (" ") + "won the toss")
-// }
+var player1Name = prompt ("enter player 1 name")
+var player2Name = prompt ("enter player 2 name")
+var tossResult = Math.round(Math.random())
+if(tossResult === 1){
+    document.write ("<h1>"+  player1Name.toUpperCase() + (" ") +"Selected HEAD and" + (" ") + "won the toss")
+}
+else{
+    document.write ("<h2>"+ player2Name.toUpperCase() + (" ") + "Selected Tail and" + (" ") + "won the toss")
+}
 // document.write (Math.ceil(tossResult))
-
+red
 /******** Question no 6 */
 
 // var rdmNum = Math.floor((Math.random() * 100) + 1);
@@ -50,11 +50,11 @@
 
 // /****** Question no 8  */
 
-var secretNum = (Math.floor(Math.random()*10) + 1);
-var userSecretnum = + prompt("enter secret number from 1 to 10")
-if (userSecretnum === secretNum){
-    alert ("Congratulations You Won")
-}
-else {
-    alert ("Try Again ")
-}
+// var secretNum = (Math.floor(Math.random()*10) + 1);
+// var userSecretnum = + prompt("enter secret number from 1 to 10")
+// if (userSecretnum === secretNum){
+//     alert ("Congratulations You Won")
+// }
+// else {
+//     alert ("Try Again ")
+// }
