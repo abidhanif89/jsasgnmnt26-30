c43b1b17c849327f4b808941059c6b8d2077b5cd
AbidHanif <qadriturabi12@gmail.com>
2021-05-26 13:57:25 +0500

    assignment completed

---
+++ app.js
@@ -0,0 +1,60 @@
+/******QUESTION NO 1 */
+// var userNum = +prompt("enter number")
+// // userNum = Math.round(userNum)
+// // userNum = Math.floor(userNum)
+// userNum = Math.ceil(userNum)
+// document.write(userNum)
+// *********************************************
+
+/*******QUESTION NO 2 */
+
+// // var userNum = +prompt("enter number")
+// // userNum = Math.round(userNum)
+// // userNum = Math.floor(userNum)
+// // userNum = Math.ceil(userNum)
+// // document.write(userNum)
+// *************************************************
+
+/********* Question No 3 */
+
+// // var userNum = +prompt("enter number")
+// // var numAbsVal = Math.abs(userNum)
+// // document.write(numAbsVal)
+// ****************************************************
+
+/************* Question no 4 */
+
+// var player1Name = prompt ("enter player 1 name")
+// var player2Name = prompt ("enter player 2 name")
+// var tossResult = Math.round(Math.random())
+// if(tossResult === 1){
+//     alert (player1Name.toUpperCase() + (" ") +"Selected HEAD and" + (" ") + "won the toss")
+// }
+// else{
+//     alert (player2Name.toUpperCase() + (" ") + "Selected Tail and" + (" ") + "won the toss")
+// }
+// document.write (Math.ceil(tossResult))
+
+/******** Question no 6 */
+
+// var rdmNum = Math.floor((Math.random() * 100) + 1);
+// document.write(rdmNum)
+// ****************************************
+
+/***** Question no 7 */
+
+// var userWeight = +prompt("enter your weight in kgs")
+// userWeight = (Math.floor(userWeight))
+// document.write(userWeight + " " + "kilograms")
+// ************************************************
+
+// /****** Question no 8  */
+
+var secretNum = (Math.floor(Math.random()*10) + 1);
+var userSecretnum = + prompt("enter secret number from 1 to 10")
+if (userSecretnum === secretNum){
+    alert ("Congratulations You Won")
+}
+else {
+    alert ("Try Again ")
+}
